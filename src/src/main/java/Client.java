8bec51b5f142ffb0ffae6565b3d322985eeaa562
Natalia H <nholea@gmail.com>
2022-10-29 18:41:04 +0200

    feat: create Client.java

---
+++ src/src/main/java/Client.java
@@ -0,0 +1,18 @@
+public class Client {
+
+        private final int id;
+
+        private final String name;
+
+        private final String surname;
+
+        private final int phoneNumber;
+
+
+    public Client(int id, String name, String surname, int phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.phoneNumber = phoneNumber;
+    }
+}
