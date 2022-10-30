aa9f4c8df1f9d97ffa1315ea71aa251412cb17ee
Natalia H <nholea@gmail.com>
2022-10-30 18:32:31 +0100

    feat: create MobilePhoneBill.java

---
+++ src/src/main/java/MobilePhoneBill.java
@@ -0,0 +1,23 @@
+import java.time.LocalDate;
+
+public class MobilePhoneBill {
+    private final Client client;
+    private final String calledNumbers;
+    private final LocalDate feeStartDate;
+    private final LocalDate feeEndDate;
+    private final Tax tax;
+
+
+    public MobilePhoneBill(Client client, String calledNumbers, LocalDate feeStartDate, LocalDate feeEndDate, Tax tax) {
+        this.client = client;
+        this.calledNumbers = calledNumbers;
+        this.feeStartDate = feeStartDate;
+        this.feeEndDate = feeEndDate;
+        this.tax = tax;
+    }
+
+
+    public void showInvoice(){
+
+    }
+}
