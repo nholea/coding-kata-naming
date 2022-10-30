88fdcef9f98aa1e180d22d90c6d13b1d0c4d5bcb
Natalia H <nholea@gmail.com>
2022-10-30 12:48:17 +0100

    feat: create Phone.java and added atributes

---
+++ src/src/main/java/PhoneCall.java
@@ -0,0 +1,19 @@
+import java.time.LocalDateTime;
+
+public class PhoneCall {
+
+    private final String sourcePhoneNumber;
+
+    private final String targetPhoneNumber;
+
+    private final LocalDateTime startedAt;
+
+    private final int minutes;
+
+    public PhoneCall(String sourcePhoneNumber, String targetPhoneNumber, LocalDateTime startedAt, int minutes) {
+        this.sourcePhoneNumber = sourcePhoneNumber;
+        this.targetPhoneNumber = targetPhoneNumber;
+        this.startedAt = startedAt;
+        this.minutes = minutes;
+    }
+}
